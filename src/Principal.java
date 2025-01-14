03be7c2a509fe463ea5612887e6c79bbf67816f8
juafracas <franxeszian@gmail.com>
2025-01-14 20:33:45 +0100

    Inicio totalSemana

--- src/Principal.java
+++ src/Principal.java
@@ -1,3 +1,39 @@
 public class Principal {
 
+    int[][] int2d = new int[5][10];
+
+
+    public int[][] multi(int[][] bid) {
+        for (int i = 0; i < bid.length; i++) {
+            for (int j = 0; j < int2d[i].length; j++) {
+                bid[i][j] *= 2;
+            }
+        }
+        return bid;
+    }
+
+
+    public String imprimir(int[][] bid) {
+
+        StringBuilder imprimir = new StringBuilder();
+
+        for (int i = 0; i < bid.length; i++) {
+            if (bid[i] == null || bid[i].length == 0) {
+                imprimir.append("Fila ").append(i).append(": vacia\n");
+            } else {
+                imprimir.append("Fila ").append(i).append(": ");
+                for (int j = 0; j < int2d[i].length; j++) {
+                    imprimir.append(bid[i][j]).append(" ");
+                }
+                imprimir.append(" ");
+            }
+        }
+
+        return imprimir.toString();
+    }
+
+
+
+
+
 }
---
+++ src/VentasSemanales.java
@@ -0,0 +1,95 @@
+import java.io.DataInput;
+
+public class VentasSemanales {
+    public enum diaSemana {
+        LUNES, MARTES, MIERCOLES, JUEVES, VIERNES, SABADO, DOMINGO
+    }
+
+    private int numSemanas;
+    private int[][] seguimiento;
+
+
+    public VentasSemanales(int numSemanas) {
+        this.numSemanas = numSemanas;
+        this.seguimiento = new int[0][7];
+    }
+
+    public void guardarVenta(int semana, diaSemana dia, int valor) {
+
+        if (semana < 0 || semana >= numSemanas) {
+            System.out.println("Semana fuera de rango.");
+        } else {
+            int indiceDia = dia.ordinal();
+            seguimiento[semana][indiceDia] = valor;
+        }
+    }
+
+
+    public void inicializarVentas(){
+        this.seguimiento = new int[0][7];
+    }
+
+    @Override
+    public String toString() {
+
+        String salida= String.format("%-11s", "Semana" + "Lunes" + "Martes" + "Miercoles" + "Jueves" + "Viernes");
+
+        for(int i = 0; i < numSemanas; i++){
+            salida += String.format("%-11s", "S" + (i+1));
+            for (int j = 0; j < 7; j++){
+                salida += String.format("%-11s", seguimiento[i][j]);
+            }
+        }
+
+        return salida;
+    }
+
+    public String totalSemana(int semana){
+        StringBuilder salida= new StringBuilder();
+
+        for (int i = 0; i < 7; i++){
+            salida.append(String.format("%-11s", seguimiento[semana][i]));
+        }
+        return salida.toString();
+    }
+
+    public int totalDiaSemana(diaSemana dia){
+
+        int suma = 0;
+
+        for (int i = 0; i < numSemanas; i++){
+            int indiceDia = dia.ordinal();
+            suma += seguimiento[i][indiceDia];
+        }
+        return suma;
+    }
+
+    public int mediaDiaSemana(diaSemana dia){
+
+        int suma = 0;
+        int media;
+
+        for(int i = 0; i < numSemanas; i++){
+            int indiceDia = dia.ordinal();
+            suma += seguimiento[i][indiceDia];
+        }
+
+        media = suma / numSemanas;
+
+        return media;
+    }
+
+    public String totalSemana(){
+        StringBuilder salida= new StringBuilder();
+
+        int suma = 0;
+
+        for (int i = 0; i < numSemanas; i++){
+            suma += seguimiento[i][i];
+            salida.append(String.format("%-11s", seguimiento[numSemanas][suma]));
+        }
+        return salida.toString();
+
+    }
+
+}
